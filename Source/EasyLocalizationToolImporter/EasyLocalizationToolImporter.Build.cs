d692c808fc43a967a69b524639461bddac9d92be
Damian Nowakowski <zompi2@gmail.com>
2026-07-04 21:52:45 +0200

    fix for crash when calling editor with -game option

--- Source/EasyLocalizationToolImporter/EasyLocalizationToolImporter.Build.cs
+++ Source/EasyLocalizationToolImporter/EasyLocalizationToolImporter.Build.cs
@@ -29,7 +29,7 @@ public class EasyLocalizationToolImporter : ModuleRules
         PublicDefinitions.RemoveAll(ECFDefinition => ECFDefinition.StartsWith("ELTIMP_"));
 
         // Disable optimization for non shipping builds (for easier debugging)
-        bool bDisableOptimization = true;
+        bool bDisableOptimization = false;
         if (bDisableOptimization && (Target.Configuration != UnrealTargetConfiguration.Shipping))
         {
             if (Target.Version.MajorVersion == 5 && Target.Version.MinorVersion >= 2)
